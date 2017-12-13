8d67143c90384555b57f09879b6b527a020787f5
hari.baskar <hari.baskar@happiestminds.com>
2017-12-13 17:51:14 +0530

    added sample preprocess methods

---
+++ preprocess.py
@@ -0,0 +1,85 @@
+import re
+import nltk
+from bs4 import BeautifulSoup
+from unidecode import unidecode
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+
+def clean_html(html):
+    """
+    Copied from NLTK package.
+    Remove HTML markup from the given string.
+
+    :param html: the HTML string to be cleaned
+    :type html: str
+    :rtype: str
+    """
+    # First we remove inline JavaScript/CSS:
+    cleaned = re.sub(r"(?is)<(script|style).*?>.*?(</\1>)", "", html.strip())
+    # Then we remove html comments. This has to be done before removing regular
+    # tags since comments can contain '>' characters.
+    cleaned = re.sub(r"(?s)<!--(.*?)-->[\n]?", "", cleaned)
+    # Next we can remove the remaining tags:
+    cleaned = re.sub(r"(?s)<.*?>", " ", cleaned)
+    # Finally, we deal with whitespace
+    cleaned = re.sub(r"&nbsp;", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    #added beautiful soup
+    soup = BeautifulSoup(cleaned,"lxml")
+    cleaned= soup.get_text()
+    cleaned=unidecode(cleaned) #decoding the symbols
+    return cleaned.strip()
+
+def review_to_wordlist( review, remove_stopwords=True ):
+    stemmer = SnowballStemmer("english")
+    # Function to convert a document to a sequence of words,
+    # optionally removing stop words.  Returns a list of words.
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(review,"html5lib").get_text()
+    #
+    # 2. Remove non-letters
+    review_text = re.sub("[^a-zA-Z]"," ", review_text)
+    #
+    # 3. Convert words to lower case and split them
+    words = review_text.lower().split()
+
+    #
+    # 4. Optionally remove stop words (false by default)
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    #remove stemming
+    words = [stemmer.stem(elem) for elem in words]
+    #
+    # 5. Return a list of words
+    return(words)
+
+# Download the punkt tokenizer for sentence splitting
+
+#nltk.download()
+
+# Load the punkt tokenizer
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+# Define a function to split a review into parsed sentences
+def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence) > 0:
+            # Otherwise, call review_to_wordlist to get a list of words
+            sentences.append( review_to_wordlist( raw_sentence, \
+              remove_stopwords ))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
